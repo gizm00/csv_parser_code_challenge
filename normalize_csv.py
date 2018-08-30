49f3bf4444f695849fc91eb9910aa414c753e233
Sev Leonard <jennifer.m.leonard@gmail.com>
2018-08-29 17:42:29 -0700

    parsed line data into fields with csv reader

--- normalize_csv.py
+++ normalize_csv.py
@@ -1,17 +1,28 @@
+
 # read in csv file on stdin, write out csv file on stdout
 # input: UTF-8
 # output: UTF-8
 import sys
 import pandas as pd
+from csv import reader, writer
 
 lines = []
+# assume the order of the columns is fixed, so we can assume the first column
+# has timestamp, the second address, etc
+# also assume that the first line is the header
+
+header = None
 for line in sys.stdin.buffer:
     line_data = line.decode('utf_8', 'replace')
-    lines.append(line_data)
-        
-with open('tmp_out.csv', 'w', encoding='utf_8') as w:
-    w.writelines(lines)
-
-df = pd.read_csv('tmp_out.csv', encoding='utf_8')
-print(df)
-
+    if header is None:
+        header = line_data
+        print(header)
+    else:
+        fields = list(reader([line_data]))[0]
+        timestamp = fields[0]
+        address = fields[1]
+        zip = fields[2]
+        fullname = fields[3]
+        fooduration = fields[4]
+        barduration = fields[5]
+        notes = fields[7]
