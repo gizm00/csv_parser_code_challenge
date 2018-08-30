47fdfa38a4065b4fbeacec1b3cdec4841e51b583
Sev Leonard <jennifer.m.leonard@gmail.com>
2018-08-29 18:17:04 -0700

    added timestamp parser

--- normalize_csv.py
+++ normalize_csv.py
@@ -3,13 +3,37 @@
 # input: UTF-8
 # output: UTF-8
 import sys
-import pandas as pd
-from csv import reader, writer
+from csv import reader
+from pytz import timezone
+from datetime import  datetime
 
-lines = []
 # assume the order of the columns is fixed, so we can assume the first column
 # has timestamp, the second address, etc
 # also assume that the first line is the header
+
+def parse_timestamp(timestamp_in):
+    """
+    convert timestamp_in to US/Eastern time and ISO-8601 format
+    returns: parsed timestamp string or None if a date cannot be converted because of a unicode issue
+    param: timestamp_in: string timestamp of m/dd/yy hh:mm:ss pm/am format representing
+    a date in US/Pacific timezone
+    """
+    # seems redundant
+    if u'\ufffd' in timestamp_in:
+        return None
+
+    try:
+        timestamp_dt_notz = datetime.strptime(timestamp_in, '%m/%d/%y %I:%M:%S %p')
+    except:
+        return None
+
+
+    # add timezone offset
+    timestamp_dt = timezone('US/Pacific').localize(timestamp_dt_notz)
+    timestamp_td_eastern = timestamp_dt.astimezone(timezone('US/Eastern'))
+    return timestamp_td_eastern.isoformat(' ')
+
+
 
 header = None
 for line in sys.stdin.buffer:
@@ -19,10 +43,11 @@ for line in sys.stdin.buffer:
         print(header)
     else:
         fields = list(reader([line_data]))[0]
-        timestamp = fields[0]
+        timestamp = parse_timestamp(fields[0])
         address = fields[1]
         zip = fields[2]
         fullname = fields[3]
         fooduration = fields[4]
         barduration = fields[5]
         notes = fields[7]
+        print("timestamp: ", timestamp)
